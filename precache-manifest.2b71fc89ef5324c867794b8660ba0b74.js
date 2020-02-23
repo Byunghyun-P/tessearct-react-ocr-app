fa3d0e52feae1072399e5c6b229a1a9b76c42dba
James Park <Byunghyun-P>
2020-02-24 01:59:23 +0900

    Updates

---
+++ precache-manifest.2b71fc89ef5324c867794b8660ba0b74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79831baffb44992bc2497d872601e7a",
+    "url": "/tessearct-react-ocr-app/index.html"
+  },
+  {
+    "revision": "3f13ae12a9de4088e147",
+    "url": "/tessearct-react-ocr-app/static/css/2.8d516ad8.chunk.css"
+  },
+  {
+    "revision": "2bec18a4c51c74621f54",
+    "url": "/tessearct-react-ocr-app/static/css/main.162e4ce1.chunk.css"
+  },
+  {
+    "revision": "3f13ae12a9de4088e147",
+    "url": "/tessearct-react-ocr-app/static/js/2.3883247c.chunk.js"
+  },
+  {
+    "revision": "c26efdc2300abe6629ffdfeb2d5a8c32",
+    "url": "/tessearct-react-ocr-app/static/js/2.3883247c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bec18a4c51c74621f54",
+    "url": "/tessearct-react-ocr-app/static/js/main.b3124010.chunk.js"
+  },
+  {
+    "revision": "ff31bc471c3c5d376f9b",
+    "url": "/tessearct-react-ocr-app/static/js/runtime-main.d1c33cba.js"
+  }
+]);
