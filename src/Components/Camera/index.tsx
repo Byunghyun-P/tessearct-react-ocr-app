658f313b0e3a681a7c7f571d453690edb701e02d
James Park <Byunghyun-P>
2020-02-24 02:32:01 +0900

    Improve usability
    - Add error message
    - Fix Tooltip position

--- src/Components/Camera/index.tsx
+++ src/Components/Camera/index.tsx
@@ -1,6 +1,7 @@
 import React, { createRef, useCallback, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Webcam from "react-webcam";
+import { Result, Typography } from "antd";
 
 import { setImage, getImage } from "../../features/ui/uiSlice";
 
@@ -8,7 +9,13 @@ import Buttons from "./Controls";
 import OcrBottomSheet from "../BottomSheet/Ocr";
 import SettingBottomSheet from "../BottomSheet/Setting";
 
-import { WebcamContainer, ImagePreviewContainer, ImagePreview } from "./styled";
+import {
+  ErrorContainer,
+  ErrorExtraInformation,
+  WebcamContainer,
+  ImagePreviewContainer,
+  ImagePreview
+} from "./styled";
 
 function Camera({}) {
   const webcamRef = createRef<any>();
@@ -18,6 +25,7 @@ function Camera({}) {
 
   const [width, setWidth] = useState<number>();
   const [height, setHeight] = useState<number>();
+  const [isMediaDeviceError, setIsMediaDeviceError] = useState<boolean>();
 
   const handleOnCapture = useCallback(() => {
     const { current: webcam } = webcamRef;
@@ -27,10 +35,51 @@ function Camera({}) {
     }
   }, [webcamRef]);
 
+  const canUseMediaDevice = useCallback(async () => {
+    try {
+      await navigator.mediaDevices.getUserMedia({
+        video: true
+      });
+      setIsMediaDeviceError(false);
+    } catch (e) {
+      console.log(e);
+      setIsMediaDeviceError(true);
+    }
+  }, []);
+
   useEffect(() => {
     setHeight(document.body.clientHeight);
     setWidth(document.body.clientWidth);
+
+    canUseMediaDevice();
   }, []);
+
+  if (isMediaDeviceError === undefined) {
+    return null;
+  }
+
+  if (isMediaDeviceError) {
+    return (
+      <ErrorContainer>
+        <Result
+          status="error"
+          title="카메라 인식 실패"
+          subTitle="카메라 사용 가능 여부 및 권한을 확인해주세요"
+        />
+        <ErrorExtraInformation>
+          <Typography.Paragraph>
+            <Typography.Text>
+              카카오톡 인앱 브라우져로 접속하셨다면, Safari / Chrome 등으로
+              실행해주세요.
+            </Typography.Text>
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            <Typography.Text> {window.navigator.userAgent} </Typography.Text>
+          </Typography.Paragraph>
+        </ErrorExtraInformation>
+      </ErrorContainer>
+    );
+  }
 
   return (
     <>
